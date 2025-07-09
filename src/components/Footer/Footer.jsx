e103b659d6df1c4c1238601f2296493f1f294ed9
zaidhanif1 <zah00005@mix.wvu.edu>
2025-07-08 20:40:44 -0400

    Spacing

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,18 +7,17 @@ import { FaLinkedin } from 'react-icons/fa'
 export default function Footer()
 {
     return(
-            <div className='footer-main-div'>
-                <div className='buy-coffee-div'>
-                    <p>Support Me!</p>
-                <a href="https://buymeacoffee.com/zaidhanif" target = '_blank' className='buy-me-coffee-a'> Made with ☕ and late nights. </a>
-                </div>
-        <div className='social-links'>
-            <p>Personal Links</p>
-            <a href="https://zaidhanif.netlify.app/" target = '_blank'><FontAwesomeIcon icon={faCircleUser} /> Portfolio</a>
-            <a href="https://github.com/zaidhanif1" target = '_blank'><FaGithub /> GitHub</a>
-            <a href="https://www.linkedin.com/in/zaidhanif/" target = '_blank'><FaLinkedin /> LinkedIn</a>
-        </div>
+        <div className='footer-main-div'>
+            <div className='buy-coffee-div'>
+                <p className='footer-heading'>Support Me!</p>
+                <a href="https://buymeacoffee.com/zaidhanif" target='_blank' className='buy-me-coffee-a'>Made with ☕ and late nights.</a>
             </div>
-
+            <div className='social-links'>
+                <p className='footer-heading'>Personal Links</p>
+                <a href="https://zaidhanif.netlify.app/" target='_blank'><FontAwesomeIcon icon={faCircleUser} /> Portfolio</a>
+                <a href="https://github.com/zaidhanif1" target='_blank'><FaGithub /> GitHub</a>
+                <a href="https://www.linkedin.com/in/zaidhanif/" target='_blank'><FaLinkedin /> LinkedIn</a>
+            </div>
+        </div>
     )
 }
